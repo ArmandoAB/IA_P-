e1bad054cf04259f7117e55f89819b1eea7a90d2
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-04 23:54:37 -0500

    Capítulo 10 Ejercicio 9

---
+++ Listas.py
@@ -0,0 +1,5 @@
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]  #se crea una lista
+print("En la posicion 3 se encuentra: " + colores[3] + ", el " + colores[0] +" se encuentra en la posicion 0 y " + colores[7] + " se encuentra en la posicion 7")    #se imprime lo que se solicito y su ubicacfion en la lista
+
+numbers = ["tres", "dos", "cinco", "cuatro", "uno"] #se crea una lista con los valores solicitados en el orden solicitado
+print("posicion 0: " + numbers[0] + "\nposicion 1: " + numbers[1] + "\nposicion 2: " + numbers[2] + "\nposicion 3: " + numbers[3] + "\nposicion 4: " + numbers[4])  #se imprime la lista
