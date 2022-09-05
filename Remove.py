cbfc2a40d0e9b8ee9f254c6823487c4100d469d4
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 00:20:57 -0500

    Capítulo 13 Ejercicio 12

---
+++ Remove.py
@@ -0,0 +1,5 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista solicitada
+print(colores)  #se muestra la lista original
+colores.remove('amarillo')  #se elimina el elemento amarillo
+colores.remove('rojo')  #se elimina el elemento rojo
+print(colores)  #se muestra el resultado
