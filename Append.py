d69886437fbc85c1c4b89a9f5f6dd96807eec3c4
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 01:43:05 -0500

    Delete Append.py

--- Append.py
+++
@@ -1,5 +0,0 @@
-colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista con los elementos solicitados
-print(colores)  #se muestra la lista
-colores.append('fuxia') #se añade el elemento fuxia
-colores.append('celeste')   #se añade el elemento celeste
-print(colores)  #se muestra la lista modificada
