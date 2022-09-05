fc60e26822f5a6d278fe8d989919c8aaa312da9e
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 00:03:13 -0500

    Capítulo 11 Ejercicio 10

---
+++ Posiciones_negativas.py
@@ -0,0 +1,2 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista con los elementos solicitados
+print(colores[-1] + ", " + colores[-2] + ", " + colores[-5] + ", " + colores[-7] + " y  " + colores[-10]) #se imprimen los elementos solicitados usando posiciones negativas para acceder a ellas
