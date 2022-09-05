211925a00081c23f5a04fbdca23ab61b4dda1a3a
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-04 22:13:48 -0500

    Capítulo 4 Ejercicio 3

---
+++ Concatenamiento.py
@@ -0,0 +1,14 @@
+SKZ1 = "We-welcome to the zone" #se crea una variable tipo string
+SKZ2 = "feeling good right now" #se crea otra variable tipo string
+SKZ3 = SKZ1 + " " + SKZ2 + " " + SKZ2  #se concatenan y guardan las variables
+print(SKZ3) #se imprime el resultado
+
+BTS = "Rain be pouring" #se crea una variable tipo string
+print(BTS + " sky keep falling everyday oh nanana") #se concatena un texto a la variable
+
+Nombre = "Armando"  #se crea una variable con mi nombre
+Apellido = "Alanis" #se crea una variable con mi apellido
+Nombre_completo = Nombre + " " + Apellido  #se concatenan las variables
+print(Nombre_completo)  #se imprime el resultado
+
+print('134' + '340')    #se concatenan dos numeros
