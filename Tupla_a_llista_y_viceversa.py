a07f61a45a823fa9207acb0f8060871f25fa0a16
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 01:51:36 -0500

    Capítulo 20 Ejercicio 18

---
+++ Tupla_a_llista_y_viceversa.py
@@ -0,0 +1,5 @@
+#Convierte la siguiente lista en una tupla y asegúrate que se haya convertido en tupla correctamente imprimiendo en la consola el tipo de elemento que es
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista
+print(colores)    #se muestra la lista y que es una lista
+tupla = tuple(colores)  #se convierte a tupla la lista
+print(tupla)    #se imprime la tupla
