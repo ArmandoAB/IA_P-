f5c7fca61598fe06a6ff793217ad71d7314b39fa
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-04 21:49:01 -0500

    Capítulo 2 Ejercicio 1

---
+++ Variables.py
@@ -0,0 +1,7 @@
+xd = "LESGOOOO" #se crea la variable y se le asigna un valor string
+print(xd)   #se imprime la variable
+
+a = 13  #se crea una variable con valor
+b = 4340    #se crea otra variable con valor
+c = (a*10000) + b   #se crea una tercera variable para guardar el valor de la operación
+print(c)    #se imprime el resultado
