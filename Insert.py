a5012eea50def7aa3f7b2160a78d355fcd859471
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 01:50:13 -0500

    Capítulo 16 Ejercicio 15

---
+++ Insert.py
@@ -0,0 +1,5 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista con los elementos solicitados
+print(colores)  #se muestra la lista
+colores.insert(-5, 'magenta')  #se inserta el elemento magenta en la posicion 6
+colores.insert(-1, 'turquesa')  #se inserta el elemento en la penultima posicion
+print(colores)  #se muestra la lista modificada
