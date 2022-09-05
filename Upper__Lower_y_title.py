73032b2fbe9956d307ede3853fad2a0c42f03a02
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-04 22:54:04 -0500

    Capítulo 5 Ejercicio 4

---
+++ Upper__Lower_y_title.py
@@ -0,0 +1,8 @@
+nombre = "enrique barros fernández".title() #se crea una frase y se usa la funcion title para poner las iniciales decada palabra en mayusculas
+print(nombre)   #se imprime el resultado
+
+frase1 = "Esta Es Una Frase Para Ser Formateada.".lower()   #se crea una frase y se usa la funcion lower para escribirlo en minusculas
+print(frase1)   #se imprime el resultado
+
+frase2 = "Esta Es Una Frase Para Ser Formateada.".upper()   #se crea una frase y se usa la funcion upper para escribirlo en mayusculas
+print(frase2)   #se imprime el resultado
