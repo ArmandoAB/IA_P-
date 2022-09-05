0f7254a32dfe22e038f304bf0523430b4d89143e
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 00:14:59 -0500

    Capítulo 12 Ejercicio 11

---
+++ Eliminar_elementos_de_listas.py
@@ -0,0 +1,7 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista con los elementos solicitados
+print(colores) #se imprime la lista original
+del(colores[-3])    #se borra el elemento 'rosa'
+del(colores[-3])    #se borra el elemento 'negro'
+del(colores[1]) #se elimina el elemento 'azul'
+del(colores[3]) #se elimina el elemento 'marrón'
+print(colores)  #se imprime la lista
