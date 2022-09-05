835eb25b7659d9205df92f27e877d1f959123b28
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 01:40:58 -0500

    Capítulo 20 Ejercicio 18

--- Tuplas.py
+++ Tuplas.py
@@ -1,6 +1,6 @@
 #Imprime la segunda posición de esta tupla
 colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja') #se crea la tupla con los elementos solicitados
-print(colores[2]) #se muestra el elemento en la posición solicitada  
+print(colores[1]) #se muestra el elemento en la posición solicitada  
 
 #Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos de la siguiente tupla en una variable llamada operacion.
 numeros = (10, 1, 5, 11)    #se crea la tupla con los elementos solicitados
