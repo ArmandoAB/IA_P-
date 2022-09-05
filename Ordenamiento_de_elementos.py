e2227b3e77da8b6bd449442a3570145df613aad0
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 01:43:45 -0500

    Delete Ordenamiento_de_elementos.py

--- Ordenamiento_de_elementos.py
+++
@@ -1,4 +0,0 @@
-colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja'] #se crea la lista con los elementos solicitados
-print(colores)  #se muestra la lista
-colores.sort(reverse=True)  #se ordena de la 'z' a la 'a'
-print(colores)  #se muestra la lista ordenada
