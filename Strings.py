949bab65f20ef9162a5870e860f4bd3798d7e2fe
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-04 21:57:31 -0500

    Capítulo 3 Ejercicio 2

---
+++ Strings.py
@@ -0,0 +1,4 @@
+xd = "No se lo que hago 'Se rie'"   #se crea una variable tipo string
+print(xd)   #imprime la variable
+xdnt = 'No se lo que hago "Llora"'  #se crea otra variable tipo string
+print(xdnt) #imprime la variable
