14ace0288773edebdf83549123f734814829a371
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 21:49:50 -0500

    Capítulo 22 Ejercicio 20

---
+++ If_else.py
@@ -0,0 +1,12 @@
+#Corrige el siguiente condicional if else
+color = 'rojo'
+
+if color == 'rojo':
+    print("El color es rojo.")
+else:
+    print("El color no es rojo.")
+
+#se reemplaza el 'else' por un 'if' ya que no puede haber un else antes que un if, además que se le agregan comillas a rojo para que detecte que es un string
+#se agregan paréntesis para mostrar el texto
+#se reemplaza el 'if' por un 'else', ya que no puede haber un else antes que un if, además se elimina la condición del else, ya qeu no lleva condicion
+#se agregan paréntesis para mostrar el texto
