03805897050bae820cb2daf778fb840197585d80
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 21:49:23 -0500

    Capítulo 21 Ejercicio 19

---
+++ If__condicionales_.py
@@ -0,0 +1,20 @@
+#Cambia el operador para que la condición sea True
+num1 = 15	#se cran dos numeros con valores diferentes
+num2 = 20
+
+if num1 < num2:	#se cambia el operador de 'igual a' a 'menor que'
+	print('Se ejecuta el if.');
+
+#Cambia el operador para que la condición sea True
+num1 = 1450	#se cran dos numeros con valores diferentes
+num2 = 60
+
+if num1 > num2:	#se cambia el poerado de 'menor que' a 'mayor que'
+	print('Se ejecuta el if.')
+
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador
+num1 = 1450	#se cran dos numeros con valores diferentes
+num2 = 6
+
+if num1 == num2:	#se cambia el operador de 'diferenta a' a 'igual a'
+	print('Se ejecuta el if.')
