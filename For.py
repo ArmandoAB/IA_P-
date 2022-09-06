b80890ecb487202d53e592bff47830c4972b5158
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 22:26:04 -0500

    Capítulo 29 Ejercicio 25

---
+++ For.py
@@ -0,0 +1,5 @@
+#Crea un bucle for que itere la siguiente tupla y muestre una frase como esta en cada iteración: 'El color es: ' + color + '.'
+colores = ('rojo','azul','verde','amarillo')
+
+for x in colores:
+    print('El color es: ' + x + '.')    #se muestran los elementos de la tupla en orden
