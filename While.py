221e2c3615aef90c4e8363bd38326a81d6a307cd
ArmandoAB <112527374+ArmandoAB@users.noreply.github.com>
2022-09-05 22:03:54 -0500

    Capítulo 27 Ejercicio 23

---
+++ While.py
@@ -0,0 +1,17 @@
+#Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5.
+x = 0   #se crea una variable con valor 0
+while x < 15:    #se comprueba si la variable es menor a 15
+    x = x + 5   #se suma 5 a la variable
+    print(x)
+
+#Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20
+x = 0   #se crea una variable con valor 0
+while x > -100:  #se comprueba si la variable es mayor a -100
+    x = x - 20  #se resta 20 a la variable
+    print(x)
+
+#Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de 1 y que muestre en cada ejecución esta frase con el valor de ejecución correspondiente: 'El valor del bucle es 10'
+x = 10  #se crea una variable con valor 0
+while x > 0:    #se comprueba si la variable es mayor a 0
+    print('El valor del bucle es ', x)
+    x = x - 1   #se resta 1 a la variable
